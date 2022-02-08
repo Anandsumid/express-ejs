3c6449c13191a9fea6bf40242dcf771c51d37cb3
lab_1_1x <lab_1_15@lab-1-15s-iMac.local>
2022-02-08 12:51:18 +0800

    haha

--- express-ejs/routes/api.js
+++ express-ejs/routes/api.js
@@ -2,24 +2,69 @@ const express = require("express");
 const app = express();
 const fs = require("fs");
 const cors = require("cors")
-
-app.get(
+const router = express.Router()
+router.get('/what', (req, res) =>{
+  res.send('haha')
+})
+router.get(
   "/user/:id",
   function (req, res, next) {
     const user_id = req.params.id;
-    if (user_id > 2000) next("route");
-    if (user_id < 50) next();
-    res.send("I will send user information #1");
+    if (user_id<10){
+      const err = new Error('Cant find user with this ID')
+      err.status = 'fail'
+      // err.statusCode = 500
+      return next(err)
+    }
+    // if (user_id > 2000) next("route");
+    // if (user_id < 50) next();
+    // res.send("I will send user information #1");
+    
   },
   function (req, res) {
-    res.send("He is a Loyal");
+    res.send("User info with id"+ user_id);
   }
 );
-app.get("/user/:id", function (req, res) {
+router.get(
+  "/user2/:id",
+  function (req, res, next) {
+    const user_id = req.params.id;
+    if (user_id<10){
+      const err = new Error('Cant find user with this ID')
+      err.status = 'faillll'
+      err.statusCode = 500
+      next(err)
+    }
+    res.send("User info with ID"+ user_id)
+  },
+  function (err, req, res, next) {
+    res.status(500).json({"message": err.status})
+  }
+);
+router.get('/user3/:id', function (req,res){
+  const user_id = req.params.id
+  if (user_id<10){
+    return res.status(500). json({"message": "something went wrong"})
+  }
+  res.send("User info with id"+ user_id)
+})
+router.get("/user/:id", function (req, res) {
   res.send("I will send user information #2");
 });
-
-app.get("/user/:id", function (req, res, next) {
+router.get('/user4/:id', function(req, res, next){
+  const user_id = req.params.id
+  if (user_id<10){
+    const err = new Error ('Cant find the fn user')
+    err.status = 'Failff'
+    err.statusCode = 500 
+    return next(err)
+  }
+  res.send ('User info with id'+ user_id)
+})
+router.use(function(err, req, res, next){
+  res.status(500).json({'err': err, "message": "Wrong", "custom": err.status})
+})
+router.get("/user/:id", function (req, res, next) {
   const user_id = req.params.id;
   if (user_id % 2 === 0) res.send("that id is EVEN");
   date = new Date(Date.now());
@@ -30,7 +75,7 @@ app.get("/user/:id", function (req, res, next) {
   );
   next("route");
 });
-app.get("/user/:id", function (req, res) {
+router.get("/user/:id", function (req, res) {
   date = new Date(Date.now());
   res.send("that id is ODD");
   fs.appendFile(
@@ -53,4 +98,4 @@ app.post("/arrayuser/:id", logStuff, function (req, res, next) {
   res.send("User Info");
 });
 
-module.exports = app
+module.exports = router
